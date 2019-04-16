b403beea0ec7cf81e2669d18df3125092676cc59
Tien Huu Le <lehuutien1996@gmail.com>
2019-04-16 15:18:21 +0700

    base-service: Moved common criteria

--- app/Repositories/Criteria/Common/WithDeletedCriteria.php
+++ app/Repositories/Criteria/Common/WithDeletedCriteria.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Repositories\Criteria;
+namespace App\Repositories\Criteria\Common;
 
 use Prettus\Repository\Contracts\CriteriaInterface;
 use Prettus\Repository\Contracts\RepositoryInterface;
